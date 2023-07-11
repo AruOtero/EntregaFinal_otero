734eab21e2d72a01c6a8c16f920f48b597f6ba65
Aru <arianaotero90@gmail.com>
2023-07-11 15:42:54 -0300

    arreglos finales

--- main.js
+++ main.js
@@ -7,49 +7,54 @@ let simular=1;
 let confirma;
 
 do{
-    importeSolicitado=Number(prompt("Ingrese el importe a solicitar"));
-    cantidadCuotas=pedirCuotas();
-    console.log(cantidadCuotas);
-    confirma= prompt("Confirma la cantidad de cuotas? \n 1- SI \n 2- NO");
-    while(confirma==2){
-        cantidadCuotas= pedirCuotas();
-        confirma=prompt("Confirma la cantidad de cuotas? \n 1-SI \n 2- NO");
+    importeSolicitado=(prompt("Ingrese el importe a solicitar"));
+    if(importeSolicitado > 0)
+        {   cantidadCuotas=pedirCuotas();
+            confirma= prompt("Confirma la cantidad de cuotas? \n 1- SI \n 2- NO");
+            while(confirma==2){
+                cantidadCuotas= pedirCuotas();
+                confirma=prompt("Confirma la cantidad de cuotas? \n 1-SI \n 2- NO");
+            }
+            diasVencimiento =pedirVencimiento();
+            console.log(diasVencimiento);
+            switch(cantidadCuotas){
+                case 1:
+                    if(diasVencimiento == 1){
+                        valorCuotas=  (((Number(importeSolicitado) + Number(impuestos))/6));
+                        console.log("El valor de la cuota es " + valorCuotas);
+                    }
+                    else{
+                        valorCuotas=  (((Number(importeSolicitado) + Number(impuestos))/6)*2);
+                        console.log("El valor de la cuota es " + valorCuotas);
+                    }
+                    break;
+                case 2:
+                    if(diasVencimiento == 1){
+                        valorCuotas =  (((Number(importeSolicitado) + Number(impuestos))/12));
+                        console.log("El valor de la cuota es " + valorCuotas);
+                    }
+                    else{
+                        valorCuotas =  (((Number(importeSolicitado) + Number(impuestos))/12)*2);
+                        console.log("El valor de la cuota es " + valorCuotas);
+                        }
+                    break;
+                case 3:
+                    if(diasVencimiento == 1){
+                        valorCuotas =  (((Number(importeSolicitado) + Number(impuestos))/12));
+                        console.log("El valor de la cuota es " + valorCuotas);
+                    }
+                    else{
+                        valorCuotas=  (((Number(importeSolicitado) + Number(impuestos))/24)*2);
+                        console.log("El valor de la cuota es " + valorCuotas);
+                    }
+                    break;
+                    }
+                alert("El valor de la cuota es: "+valorCuotas);
+                simular=prompt("Seleccione la opción requerida \n 1 - Simular otro importe \n 2- Salir");
+        }
+    else{
+        simular==2;
     }
-    diasVencimiento =pedirVencimiento();
-    console.log(diasVencimiento);
-    switch(cantidadCuotas){
-        case 1:
-            if(diasVencimiento == 1){
-                valorCuotas=  (((Number(importeSolicitado) + Number(impuestos))/6));
-                console.log("El valor de la cuota es " + valorCuotas);
-            }
-            else{
-                valorCuotas=  (((Number(importeSolicitado) + Number(impuestos))/6)*2);
-                console.log("El valor de la cuota es " + valorCuotas);
-            }
-            break;
-        case 2:
-            if(diasVencimiento == 1){
-                valorCuotas =  (((Number(importeSolicitado) + Number(impuestos))/12));
-                console.log("El valor de la cuota es " + valorCuotas);
-            }
-            else{
-                valorCuotas =  (((Number(importeSolicitado) + Number(impuestos))/12)*2);
-                console.log("El valor de la cuota es " + valorCuotas);
-                }
-            break;
-        case 3:
-            if(diasVencimiento == 1){
-                valorCuotas =  (((Number(importeSolicitado) + Number(impuestos))/12));
-                console.log("El valor de la cuota es " + valorCuotas);
-            }
-            else{
-                valorCuotas=  (((Number(importeSolicitado) + Number(impuestos))/24)*2);
-                console.log("El valor de la cuota es " + valorCuotas);
-            }
-            break;
-            }
-        alert("El valor de la cuota es: "+valorCuotas);
-        simular=prompt("Seleccione la opción requerida \n 1 - Simular otro importe \n 2- Salir");
+    
 
 }while (simular==1);
