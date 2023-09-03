47ed8493f745b9b2d60ea5d0f8e0d75b4c47898f
Aru <arianaotero90@gmail.com>
2023-09-02 23:10:41 -0300

    agrego app

---
+++ app.js
@@ -0,0 +1,8 @@
+const data = {
+    verCuota: verCuota,
+    interes: 2.5,
+  };
+  
+  // Convertir el objeto a una cadena JSON
+  const jsonData = JSON.stringify(data);
+  
--- main.js
+++ main.js
@@ -1,40 +1,38 @@
 let verCuota;
-document.getElementById("formu");
-addEventListener("submit",validarFormulario);    
-
-function validarFormulario (e){
-    let importe = Number(document.getElementById("importe").value);
-    let cuotas = Number(document.getElementById("cuotas").value); 
-    let vencimiento = Number(document.getElementById("vencimiento").value); 
-    e.preventDefault();
-    console.log("el importe es"+importe);
-    console.log(typeof(importe));
-    console.log("las cuotas son" + cuotas);    
-    console.log(typeof(cuotas));
-    console.log("el vencimiento es en" + vencimiento + "dias");
-    console.log(typeof(vencimiento));
-    if(vencimiento === 30){
-        verCuota= importe/Number(cuotas);}
-        else{
-            verCuota= importe/Number(cuotas)*2;
-        };
-        console.log(verCuota);  
-};
+document.getElementById("formu").addEventListener("submit", validarFormulario);
 
 
 
 
 
-addEventListener('submit', () =>{
-    
-    Swal.fire({
-        title: 'el valor de la cuota es '+ verCuota,
-        confirmButtonText: 'Salir'
-    })
+function validarFormulario(e) {
+  let importe = Number(document.getElementById("importe").value);
+  let cuotas = Number(document.getElementById("cuotas").value);
+  let vencimiento = Number(document.getElementById("vencimiento").value);
+  e.preventDefault();
+  console.log("el importe es" + importe);
+  console.log(typeof importe);
+  console.log("las cuotas son" + cuotas);
+  console.log(typeof cuotas);
+  console.log("el vencimiento es en" + vencimiento + "dias");
+  console.log(typeof vencimiento);
+  const data = JSON.parse(jsonData);
+  const interes = data.interes;
+  if (vencimiento === 30) {
+    verCuota = (importe+interes) / Number(cuotas);
+  } else {
+    verCuota = (importe+interes) / Number(cuotas) * 2;
+  }
+  console.log(verCuota);
 
-})
+  // Guardar verCuota en localStorage
+  localStorage.setItem("verCuota", verCuota);
+}
 
-
-
-
+document.getElementById("formu").addEventListener("submit", () => {
+  Swal.fire({
+    title: "el valor de la cuota es " + verCuota,
+    confirmButtonText: "Salir"
+  });
+});
 
