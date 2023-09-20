64b8c1f46443d352baed55c5d4936f50f06a9e0c
Aru <arianaotero90@gmail.com>
2023-09-20 13:55:24 -0300

    corregido

--- app.js
+++ app.js
@@ -1,16 +1,65 @@
-const data = {
-    verCuota: verCuota,
-    interes: 2.5,
-  };
-  
-  // Convertir el objeto a una cadena JSON
-  const jsonData = JSON.stringify(data);
+//validar formulario 
+function validarFormulario(e) {
+    e.preventDefault();
+    
+    // Obtener valores 
+    const importe = Number(importeInput.value);
+    const cuotas = Number(cuotasInput.value);
+    const vencimiento = Number(vencimientoInput.value);
+    const nombre = nombreInput.value;
 
-// Crear un objeto de usuario
-let usuario = {
-  nombre: "",
-  apellido: "",
-};
+    // Ver si hay datos cargados
+    if (isNaN(importe) || isNaN(cuotas) || isNaN(vencimiento)) {
+        popUp("Ingrese valores válidos para calcular");
+    } else {
+        verCuota(importe, cuotas, vencimiento, nombre);
+    }
+}
+// calcular cuota con los valores ingresados
+async function verCuota(a, b, c, nombre) {
+    try {
+        // interés por nombre(busca en json)
+        const interes = await interesPorNombre(nombre);
+        //valida que los días de vencimiento sea igual a 30, sino paga el doble
+        const valor = (c === 30) ? (a + interes) / b : (a + interes) / (b * 2);
 
-// Crear un array para almacenar usuarios
-let usuarios = [];
+        // Guarda el valor de la cuota en el almacenamiento local
+        guardarLocal(valor);
+
+        popUp(`El valor de la cuota es ${valor}`);
+    } catch (error) {
+        popUp("Error al obtener los datos del servidor");
+    }
+}
+
+// obtener el interés por nombre desde un archivo JSON
+async function interesPorNombre(nombre) {
+    const response = await fetch('datosDatos.json');
+    const data = await response.json();
+    const objetoBuscado = data.find(item => item.nombre === nombre);
+
+    if (objetoBuscado) {
+        return objetoBuscado.interes;
+    } else {
+        throw new Error("Nombre no encontrado en el JSON");
+    }
+}
+
+// guarda datos en el local
+function guardarLocal(verCuota) {
+    const data = {
+        verCuota: verCuota,
+        importe: importeInput.value,
+        cuotas: cuotasInput.value,
+        vencimiento: vencimientoInput.value
+    };
+    const jsonData = JSON.stringify(data);
+    localStorage.setItem("prestamoData", jsonData);
+}
+
+function popUp(mensaje) {
+    Swal.fire({
+        title: mensaje,
+        confirmButtonText: "Salir"
+    });
+}
--- main.js
+++ main.js
@@ -1,36 +1,7 @@
-let verCuota;
+const importeInput = document.getElementById("importe");
+const cuotasInput = document.getElementById("cuotas");
+const vencimientoInput = document.getElementById("vencimiento");
+const nombreInput = document.getElementById("nombre");
+const formulario = document.getElementById("formu");
 
-document.getElementById("formu").addEventListener("submit", validarFormulario);
-
-function validarFormulario(e) {
-  e.preventDefault();
-  let importe = Number(document.getElementById("importe").value);
-  let cuotas = Number(document.getElementById("cuotas").value);
-  let vencimiento = Number(document.getElementById("vencimiento").value);
-
-
-  fetch('https://my.api.mockaroo.com/users.json')
-    .then(response => response.json())
-    .then(data => {
-      const interes = data.interes;
-
-      if (vencimiento === 30) {
-        verCuota = (importe + interes) / cuotas;
-      } else {
-        verCuota = (importe + interes) / cuotas * 2;
-      }
-      console.log(verCuota);
-
-      // Guardar verCuota en localStorage
-      localStorage.setItem("verCuota", verCuota);
-
-      Swal.fire({
-        title: "El valor de la cuota es " + verCuota,
-        confirmButtonText: "Salir"
-      });
-    })
-    .catch(error => {
-      console.error("Error al obtener datos del servidor:", error);
-      // Puedes manejar el error aquí, por ejemplo, mostrar un mensaje de error al usuario.
-    });
-}
+formulario.addEventListener("submit", validarFormulario);
